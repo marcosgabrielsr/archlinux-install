6e2f063e77c36f8533863b9b30970100fd5f9dfd
marcosdevse <marcosdevse@gmail.com>
2025-11-02 14:48:23 -0400

    feat: File to define and configure datetime services

---
+++ time_date_config.sh
@@ -0,0 +1,11 @@
+set_timezone() {
+    ln -sf /usr/share/zoneinfo/$TIMEZONE
+}
+
+synchronize_clock() {
+    hwclock --systohc                               # sincroniza relógio da máquina com relógio do sistema
+}
+
+synchronize_internet_clock() {
+    timedatectl set-ntp true                        # sincroniza o relógio local com o da internet
+}
